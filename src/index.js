5ea40eb6cb1e1b12632e68df425db789cadf0930
georgios1985 <giorgos.soupionis@gmail.com>
2026-02-10 14:46:36 +0100

    Initialize React app with basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const App = () => (
+  <div style={{backgroundColor: '#050505', color: '#d4af37', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>
+    <h1>ULTRAVIST 8K<br/>COMING SOON</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
